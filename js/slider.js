6c12b98c5cb47fb1a88553a6cedb38669fe5e137
Muguckz <fedemuguruza@gmail.com>
2021-12-02 09:45:35 -0300

    Se mejoró el slider con animación de entrada y salida y se pusieron bien las descripciones de los combos

--- js/slider.js
+++ js/slider.js
@@ -1,18 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    const sliderImages = [];
-
     const setUpSlider = () => {
-        sliderImages[0] = '../assets/images/promocion-2.webp';
-        sliderImages[1] = '../assets/images/promocion-1.webp';
+        const path = 'assets/images';
+    
+        const promotions = [
+            {
+                description: 'MEGA NOT BURGER CLASSIC + PAPAS FRITAS + COCA COLA',
+                image: `${path}/promocion-2.webp`
+            },
+            {
+                description: 'MEGA NOT BURGER DELUXE + PAPAS FRITAS',
+                image: `${path}/promocion-1.webp` 
+            }
+        ]
+        return promotions;
     }
 
+    const setAnimations = (children) => {
+        children.forEach(element => {
+            element.classList.remove("slideout");
+            element.classList.add("slidein");
+            setTimeout(() => {
+                element.classList.remove("slidein");
+                element.classList.add("slideout");
+            }, 5000)
+        });
+    }
+
+    const promotions = setUpSlider();
+
     let index = 0;
-    const timer = 5000;
-    const slider = document.querySelector(".slider")
+    const sliderImg = document.querySelector("#slider-img")
+    const description = document.querySelector("#description");
+    const children = [].slice.call(document.querySelector("#slider").children);
+
+    setAnimations(children);
 
     const changeImages = () => {
-        slider.firstElementChild.src = sliderImages[index];
+        sliderImg.src = promotions[index].image;
+        description.innerHTML = promotions[index].description;
+
+        setAnimations(children);
         
         if (index < 1)
             index++;
@@ -20,6 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
             index = 0; 
     }
 
-    setUpSlider();
+    const timer = 6000;
     setInterval(changeImages, timer);
 });
