86cc9532501f61acee8d4cd0956dacb77da8f3ae
Muguckz <fedemuguruza@gmail.com>
2021-12-04 12:10:38 -0300

    Se agregó la lista de productos y un smooth scroll al navbar

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const foodContainer = document.querySelector("#food-container");
     const hamburguesa = foodContainer.querySelector(".hamburguesa");
-    // const manos = foodContainer.querySelector(".manos");
     const plato = foodContainer.querySelector(".plato");
 
     const scroll = () => {
@@ -11,12 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
             let value = window.scrollY;
             
             if (value <= 550) {
-                // if (value > 300) {
-                    // manos.style.marginTop = value * -0.5 + "px";
                 hamburguesa.style.marginTop = value * 0.6 + "px";
                 console.log(value * 0.6);
                 plato.style.marginTop = value * -0.5 + "px";
-                // }
             }
         })
     }
