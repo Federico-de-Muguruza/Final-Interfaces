97e1d7a6d5cab6ba32530359ba35978b24530525
Muguckz <fedemuguruza@gmail.com>
2021-12-06 13:47:05 -0300

    Se agregó la versión mobile

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    mobileMenu = document.querySelector("#mobileMenu");
+    document.querySelector("#dropdown").addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
+
+});
