192baf0ffd4fc2b0d9a456ccda25fae8ba0793d1
Muguckz <fedemuguruza@gmail.com>
2021-12-06 19:09:36 -0300

    Se mejoró el navbar para que tenga su icono intuitivo para cerrar el dropdown en la versión mobile y para que también se cierre haciendo clic en cualquier otro lado que no sea el dropdown

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    mobileMenu = document.querySelector("#mobileMenu");
-    document.querySelector("#dropdown").addEventListener("click", () => {
+    const mobileMenu = document.querySelector("#mobileMenu");
+    const dropdown = document.querySelector("#dropdown");
+
+    dropdown.addEventListener("click", () => {
+
         mobileMenu.classList.toggle("hidden");
+
+        if (dropdown.classList.contains("fa-bars")) {
+            dropdown.classList.add("fa-window-close");
+            dropdown.classList.remove("fa-bars");
+        } else {
+            dropdown.classList.remove("fa-window-close");
+            dropdown.classList.add("fa-bars");
+        }
+
     });
+
+    document.addEventListener("click", (e) => {
+
+        if (e.target != mobileMenu && e.target != dropdown) {
+            mobileMenu.classList.add("hidden");
+            dropdown.classList.remove("fa-window-close");
+            dropdown.classList.add("fa-bars");
+        }
+
+    });
+
 });
