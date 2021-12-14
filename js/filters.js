b7c8c529f84455c411c285939b0ec6bc336d8248
Muguckz <fedemuguruza@gmail.com>
2021-12-13 21:06:34 -0300

    Se agregó el precio a la lista de productos y se agregó una animación a los mismos

--- js/filters.js
+++ js/filters.js
@@ -5,32 +5,38 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             'title': 'MEGA NOT BURGUER DELUXE',
             'description': 'Con queso, carne y mostaza',
-            'image': `${path}/hamburguesa.png`
+            'image': `${path}/hamburguesa.png`,
+            'price': '$499'
         },
         {
             'title': 'MEGA NOT BURGER CLASSIC',
             'description': 'Con pollo, tomate y lechuga',
-            'image': `${path}/hamburguesa-pollo.png`
+            'image': `${path}/hamburguesa-pollo.png`,
+            'price': '$399'
         },
         {
             'title': 'HELADO DE FRUTILLA',
             'description': 'Con cono de azúcar',
-            'image': `${path}/helado.png`
+            'image': `${path}/helado.png`,
+            'price': '$250'
         },
         {
             'title': 'ENSALADA DE ATÚN',
             'description': 'Con huevo de verduras y vegetales de hoja',
-            'image': `${path}/ensalada.png`
+            'image': `${path}/ensalada.png`,
+            'price': '$225'
         },
         {
             'title': 'CAJITA',
             'description': 'Con juguete sorpresa, papas fritas y hamburguesa',
-            'image': `${path}/cajita.png`
+            'image': `${path}/cajita.png`,
+            'price': '$899'
         },
         {
             'title': 'CAFÉ EXPRESSO',
             'description': '',
-            'image': `${path}/cafe.png`
+            'image': `${path}/cafe.png`,
+            'price': '$325'
         },
     ]
 
@@ -51,23 +57,27 @@ document.addEventListener("DOMContentLoaded", () => {
     
     const productsImage = document.querySelectorAll(".product");
     const productsDescription = document.querySelectorAll(".product-description");
+    const productsTitlePrice = document.querySelectorAll(".product-title-price");
     const changeProductsInformation = (product, allProducts) => {
+        
         if (allProducts) {
             for (let i = 0; i < productsImage.length; i++) {
                 productsImage[i].src = product[i].image;
-                productsDescription[i].firstElementChild.innerHTML = products[i].title;
+                productsTitlePrice[i].firstElementChild.innerHTML = products[i].title;
+                productsTitlePrice[i].lastElementChild.innerHTML = products[i].price;
                 productsDescription[i].lastElementChild.innerHTML = products[i].description;
             }  
         } else {
             for (let i = 0; i < productsImage.length; i++) {
                 productsImage[i].src = product.image;
-                productsDescription[i].firstElementChild.innerHTML = product.title;
+                productsTitlePrice[i].firstElementChild.innerHTML = product.title;
+                productsTitlePrice[i].lastElementChild.innerHTML = product.price;
                 productsDescription[i].lastElementChild.innerHTML = product.description;
             }  
         }
     }
 
-    lastBtn = document.querySelector("#all-products");
+    let lastBtn = document.querySelector("#all-products");
     const changeProducts = (e) => {
         if (e.target.id === "all-products") {
             changeProductsInformation(products, true);
